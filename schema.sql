bc40da096703997c59cc80cc4affdfeb81b28eee
Ambrose Kibet <ambrosekibet36@gmail.com>
2023-04-17 23:14:38 +0300

    add email indexing

--- schema.sql
+++ schema.sql
@@ -55,5 +55,6 @@ ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
  CREATE INDEX owners_idx ON owners (id);
  CREATE INDEX visits_vet_id_idx ON visits (vet_id);
+ CREATE INDEX emails ON owners(email);
  
  
